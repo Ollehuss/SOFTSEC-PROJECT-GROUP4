c970862dacc250dc932016f0c2c94bf63ba48a1f
Ollehuss <127675229+Ollehuss@users.noreply.github.com>
2026-09-14 11:25:37 +0200

    Update unsafe_bash_bridge_append_eof.py
    Remove shell execution from bash-bridge watermarking method (OS-command)
    
    Co-Authored-By: idaslanjankic <323973691+idaslanjankic@users.noreply.github.com>
    Co-Authored-By: Joshua Han <102590577+joshiiii12345@users.noreply.github.com>

--- server/src/unsafe_bash_bridge_append_eof.py
+++ server/src/unsafe_bash_bridge_append_eof.py
@@ -8,7 +8,6 @@ any watermarking implementation this way. Don't, unless you know how to sanitize
 from __future__ import annotations
 
 from typing import Final
-import subprocess
 
 from watermarking_method import (
     InvalidKeyError,
@@ -47,11 +46,8 @@ class UnsafeBashBridgeAppendEOF(WatermarkingMethod):
         ignored by this method.
         """
         data = load_pdf_bytes(pdf)
-        cmd = "cat " + str(pdf.resolve()) + " &&  printf \"" + secret + "\""
-        
-        res = subprocess.run(cmd, shell=True, check=True, capture_output=True)
-        
-        return res.stdout
+       
+        return data + secret.encode("utf-8")
         
     def is_watermark_applicable(
         self,
